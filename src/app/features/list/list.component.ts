e34cd1b18095de26e44329e02e2053038176c6d9
Isabella Gouveia <isabellagouveias@gmail.com>
2024-05-28 22:18:49 -0300

    adiciona link para criar um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { ProductsService } from '../../shared/services/products.service';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 import { Product } from '../../shared/interfaces/product.interface';
+import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -19,4 +21,5 @@ export class ListComponent {
       this.products = products;
     });
   }
+
 }
